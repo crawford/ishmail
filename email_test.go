52cf4fd90056dd75f04fb0f0afbe75ee775148a4
Alex Crawford <adrofwarc@gmail.com>
2014-04-25 01:18:08 -0700

    Improving tests

--- email_test.go
+++ email_test.go
@@ -17,37 +17,173 @@ limitations under the License.
 package ishmail
 
 import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/mail"
 	"testing"
 	"time"
-	"net/mail"
 )
 
-func TestHeadersToString(t *testing.T) {
-	tests := []struct {
-		input  map[string]string
-		output string
-	}{
-		{
-			input:  nil,
-			output: "",
-		},
-		{
-			input: map[string]string{
-				"From":         "test1@example.com",
-				"To":           "test2@example.com",
-				"Subject":      "Testing",
-				"MIME-Version": "1.0",
-				"Content-Type": "text/html",
-				"Date":         time.Date(2014, time.March, 29, 2, 32, 17, 1232, time.UTC).Format(time.RFC1123Z),
-			},
-			output: "From: test1@example.com\r\nTo: test2@example.com\r\nSubject: Testing\r\nMIME-Version: 1.0\r\nContent-Type: text/html\r\nDate: Sat, 29 Mar 2014 02:32:17 +0000\r\n",
-		},
+type TestEmail struct {
+	from        *mail.Address
+	to          []*mail.Address
+	subject     string
+	contentType string
+	body        string
+	bodyErr     error
+	date        time.Time
+}
+
+func (e TestEmail) From() *mail.Address {
+	return e.from
+}
+
+func (e TestEmail) To() []*mail.Address {
+	return e.to
+}
+
+func (e TestEmail) Subject() string {
+	return e.subject
+}
+
+func (e TestEmail) ContentType() string {
+	return e.contentType
+}
+
+func (e TestEmail) Body() (string, error) {
+	return e.body, e.bodyErr
+}
+
+func (e TestEmail) Date() time.Time {
+	return e.date
+}
+
+func TestGenerateEmailNil(t *testing.T) {
+	if _, err := generateMessage(nil); err == nil {
+		t.FailNow()
 	}
-	for _, test := range tests {
-		got := headersToString(test.input)
-		if got != test.output {
-			t.Errorf("Expected:\n'''%s'''\n\nbut got:\n'''%s'''", test.output, got)
+}
+
+func TestGenerateEmailNilFrom(t *testing.T) {
+	email := TestEmail{
+		from: nil,
+		to:   []*mail.Address{},
+	}
+
+	_, err := generateMessage(email)
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestGenerateEmailNilTo(t *testing.T) {
+	email := TestEmail{
+		from: &mail.Address{},
+		to:   nil,
+	}
+
+	_, err := generateMessage(email)
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestGenerateEmailBodyErr(t *testing.T) {
+	bodyErr := errors.New("Test")
+	email := TestEmail{
+		from:    &mail.Address{},
+		to:      []*mail.Address{},
+		bodyErr: bodyErr,
+	}
+
+	_, err := generateMessage(email)
+	if err != bodyErr {
+		t.FailNow()
+	}
+}
+
+func TestGenerateEmail(t *testing.T) {
+	email := TestEmail{
+		from: &mail.Address{
+			Name:    "Test One",
+			Address: "test1@example.com",
+		},
+		to: []*mail.Address{
+			{
+				Name:    "Test Two",
+				Address: "test2@example.com",
+			},
+			{
+				Name:    "Test Three",
+				Address: "test3@example.com",
+			},
+		},
+		subject:     "Test Email",
+		contentType: "text/html",
+		body:        "Hello.",
+		date:        time.Time{},
+	}
+	expect := map[string][]string{
+		"From":         []string{email.from.String()},
+		"To":           []string{email.to[0].String() + ", " + email.to[1].String()},
+		"Subject":      []string{"Test Email"},
+		"Mime-Version": []string{"1.0"},
+		"Content-Type": []string{"text/html"},
+		"Date":         []string{"Mon, 01 Jan 0001 00:00:00 +0000"},
+	}
+
+	rawMessage, err := generateMessage(email)
+	if err != nil {
+		t.Fatalf("Failed to generate message (%s)", err)
+	}
+
+	message, err := mail.ReadMessage(bytes.NewBuffer(rawMessage))
+	if err != nil {
+		t.Fatalf("Failed to read message (%s)", err)
+	}
+
+	body, err := ioutil.ReadAll(message.Body)
+	if err != nil {
+		t.FailNow()
+	}
+	if !bytes.Equal(body, []byte(email.body)) {
+		t.Fatal("Body is wrong")
+	}
+
+	if len(expect) != len(message.Header) {
+		t.Fatal("Header length is wrong")
+	}
+	for k, v := range expect {
+		if len(v) != len(message.Header[k]) {
+			t.Fatalf("Header entry is wrong (%s:%s)", k, v)
 		}
+		for i, s := range v {
+			if s != message.Header[k][i] {
+				t.Fatalf("Header entry is wrong (%s:%s)", k, v, message.Header[k][i])
+			}
+		}
+	}
+}
+
+func TestHeadersToStringNil(t *testing.T) {
+	if headersToString(nil) != "\r\n" {
+		t.FailNow()
+	}
+}
+
+func TestHeadersToString(t *testing.T) {
+	headers := map[string]string{
+		"From":         "test1@example.com <Test Person>",
+		"To":           "test2@example.com",
+		"Subject":      "Testing",
+		"MIME-Version": "1.0",
+		"Content-Type": "text/html",
+		"Date":         time.Date(2014, time.March, 29, 2, 32, 17, 1232, time.UTC).Format(time.RFC1123Z),
+	}
+	expect := "From: test1@example.com <Test Person>\r\nTo: test2@example.com\r\nSubject: Testing\r\nMIME-Version: 1.0\r\nContent-Type: text/html\r\nDate: Sat, 29 Mar 2014 02:32:17 +0000\r\n\r\n"
+	if headersToString(headers) != expect {
+		t.FailNow()
 	}
 }
 
@@ -97,46 +233,6 @@ func TestAddrsToAddresses(t *testing.T) {
 			if result != test.output[i] {
 				t.Errorf("Expected '%s' but got '%s'", test.output[i], result)
 			}
-		}
-	}
-}
-
-func TestAddrsToString(t *testing.T) {
-	tests := []struct {
-		input  []*mail.Address
-		output string
-	}{
-		{
-			input:  []*mail.Address{},
-			output: "",
-		},
-		{
-			input: []*mail.Address{
-				&mail.Address{
-					Name:    "First Last",
-					Address: "test1@example.com",
-				},
-			},
-			output: "=?utf-8?q?First_Last?= <test1@example.com>",
-		},
-		{
-			input: []*mail.Address{
-				&mail.Address{
-					Name:    "First Last",
-					Address: "test1@example.com",
-				},
-				&mail.Address{
-					Name:    "A B",
-					Address: "test2@example.com",
-				},
-			},
-			output: "=?utf-8?q?First_Last?= <test1@example.com>, =?utf-8?q?A_B?= <test2@example.com>",
-		},
-	}
-	for _, test := range tests {
-		got := addrsToString(test.input)
-		if got != test.output {
-			t.Errorf("Expected '%s' but got '%s'", test.output, got)
 		}
 	}
 }
